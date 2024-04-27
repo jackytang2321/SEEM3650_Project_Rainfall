3a6ef03958282c6f581b640b5349ccaec8224398
jackytang2321 <166300899+jackytang2321@users.noreply.github.com>
2024-04-27 19:56:25 +0800

    Update multi_linear_regression.py

--- multi_linear_regression.py
+++ multi_linear_regression.py
@@ -3,43 +3,55 @@ import pandas as pd
 import datetime
 import matplotlib.pyplot as plt
 from sklearn.linear_model import LinearRegression
-from mpl_toolkits.mplot3d import Axes3D
-# from sklearn.model_selection import KFold
+from sklearn.model_selection import KFold
+from sklearn.metrics import mean_squared_error
 
 rainfall = pd.read_csv('annual_mean_daily_rainfall.csv')
 sea_temp = pd.read_csv('annual_mean_sea_temperature.csv')
 carbon = pd.read_csv('annual_carbon_emissions.csv')
+y = rainfall.iloc[:25,1].values
+x1 = sea_temp.iloc[:25,1].values
+x2 = carbon.iloc[:25,1].values
 
-y = rainfall.iloc[:,1].values
-x1 = sea_temp.iloc[:,1].values
-x2 = carbon.iloc[:,1].values
+# KFold
+x = np.vstack([x1, x2]).T
+LR = LinearRegression(fit_intercept=False)
+kcv = KFold(n_splits=5, random_state=60, shuffle=True)
+MSE = []
+for train_index, test_index in kcv.split(x):
+    # split the data
+    X_train, X_test = x[train_index], x[test_index]
+    y_train, y_test = y[train_index], y[test_index]
+    #fit the model
+    LR.fit(X_train, y_train)
+    MSE.append(mean_squared_error(y_test, LR.predict(X_test)))
+print(f'Multi-Linear Regression MSE: {np.mean(MSE):.5f}' + \
+      f', STD: {np.std(MSE):.5f}')
 
-# x = np.concatenate([x1.reshape(-1, 1), x2.reshape(-1, 1)], axis=1)
+# Prediction
+hist_y = rainfall.iloc[25:,1].values
+new_x1 = sea_temp.iloc[25:,1].values
+new_x2 = carbon.iloc[25:,1].values
+new_x = np.vstack([new_x1, new_x2]).T
+pred_y = []
+for i in range(5):
+    pred = LR.predict(new_x[i, :].reshape(1, -1))
+    pred_y.append(round(pred[0], 2))
+pred_MSE = mean_squared_error(hist_y, pred_y)
+print(f'Predicted Y (2018-2022) = {pred_y}')
+print(f'History Data of Y (2018-2022) = {hist_y}')
+print(f'Predicted Y (From LR) MSE: {pred_MSE:.5f}')
 
-# Create the plot and Add the data points
+# Create 3d-graph
 fig = plt.figure()
 ax = fig.add_subplot(111, projection='3d')
 ax.scatter(x1, x2, y)
-
-# Fit a plane using np.linalg.lstsq
-x = np.vstack([x1, x2]).T
 plane_coef, _, _, _ = np.linalg.lstsq(x, y, rcond=None)
-
-# Linear Regression
-LR = LinearRegression(fit_intercept=False)
-LR.fit(x, y)
-print(LR.coef_)
-
-# Create planes
 x1_plane, x2_plane = np.meshgrid(x1, x2)
 y_plane = plane_coef[0] * x1_plane + plane_coef[1] * x2_plane
-
-# Add the regression plane
 ax.plot_surface(x1_plane, x2_plane, y_plane, alpha=0.5)
-
-# Add labels and title
 ax.set_xlabel('Sea Temperature (Celsius)')
-ax.set_ylabel('Total Greenhouse Gas emissions (kilotonnes CO2-e)')
+ax.set_ylabel('Total Greenhouse Gas emissions\n(kilotonnes CO2-e)')
 ax.set_zlabel('Annual Mean Daily Rainfall (mm)')
 plt.title('Multiple Linear Regression')
 plt.show()
