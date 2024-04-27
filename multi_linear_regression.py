7f96c23736adda93e125b7e503ebd8eea08c7c14
jackytang2321 <166300899+jackytang2321@users.noreply.github.com>
2024-04-27 20:07:02 +0800

    Update multi_linear_regression.py

--- multi_linear_regression.py
+++ multi_linear_regression.py
@@ -1,6 +1,5 @@
 import numpy as np
 import pandas as pd
-import datetime
 import matplotlib.pyplot as plt
 from sklearn.linear_model import LinearRegression
 from sklearn.model_selection import KFold
@@ -9,9 +8,9 @@ from sklearn.metrics import mean_squared_error
 rainfall = pd.read_csv('annual_mean_daily_rainfall.csv')
 sea_temp = pd.read_csv('annual_mean_sea_temperature.csv')
 carbon = pd.read_csv('annual_carbon_emissions.csv')
-y = rainfall.iloc[:25,1].values
-x1 = sea_temp.iloc[:25,1].values
-x2 = carbon.iloc[:25,1].values
+y = rainfall.iloc[:25, 1].values
+x1 = sea_temp.iloc[:25, 1].values
+x2 = carbon.iloc[:25, 1].values
 
 # KFold
 x = np.vstack([x1, x2]).T
@@ -19,19 +18,17 @@ LR = LinearRegression(fit_intercept=False)
 kcv = KFold(n_splits=5, random_state=60, shuffle=True)
 MSE = []
 for train_index, test_index in kcv.split(x):
-    # split the data
     X_train, X_test = x[train_index], x[test_index]
     y_train, y_test = y[train_index], y[test_index]
-    #fit the model
     LR.fit(X_train, y_train)
     MSE.append(mean_squared_error(y_test, LR.predict(X_test)))
 print(f'Multi-Linear Regression MSE: {np.mean(MSE):.5f}' + \
       f', STD: {np.std(MSE):.5f}')
 
 # Prediction
-hist_y = rainfall.iloc[25:,1].values
-new_x1 = sea_temp.iloc[25:,1].values
-new_x2 = carbon.iloc[25:,1].values
+hist_y = rainfall.iloc[25:, 1].values
+new_x1 = sea_temp.iloc[25:, 1].values
+new_x2 = carbon.iloc[25:, 1].values
 new_x = np.vstack([new_x1, new_x2]).T
 pred_y = []
 for i in range(5):
